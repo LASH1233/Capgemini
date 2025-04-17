ba70417d2c1e59706e6872dfe167fa2bd57e908b
LASH1233 <113049176+LASH1233@users.noreply.github.com>
2025-04-17 13:54:02 +0530

    Add files via upload

---
+++ linkedlist3/Demo.java
@@ -0,0 +1,18 @@
+package linkedlist3;
+
+import java.util.LinkedList;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(10);
+		list.add(20);
+		System.out.println(list);
+		System.out.println(list.peek());
+		System.out.println(list.contains(10));
+		
+		
+	}
+	
+}
